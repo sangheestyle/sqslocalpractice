16750c73d5f7df7688990975aa051cd73d18e437
Sanghee Kim <sangheestyle@gmail.com>
2018-07-12 01:00:37 +0300

    DRAFT 2
    $ docker-compose up --scale tenantreader=2 --build --force-recreate

--- api/server.js
+++ api/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const AWS = require('aws-sdk');
 
 const PORT = process.env.PORT || 8080;
 const HOST = '0.0.0.0';
+
+const myCredentials = new AWS.Credentials("x", "x");
+const sqs = new AWS.SQS({
+    apiVersion: '2012-11-05',
+    credentials: myCredentials,
+    region: "none",
+    endpoint: "http://sqs:9324"
+});
+const queueURL = "http://sqs:9324/queue/newtenants";
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +20,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello world\n');
+});
+
+app.post('/tenants', (req, res) => {
+    const messageBody = {
+        name: req.body["name"],
+    };
+    const params = {
+        DelaySeconds: 2,
+        MessageAttributes: {
+            "MessageBodyType": {
+                DataType: "String",
+                StringValue: "JSON"
+            },
+        },
+        MessageBody: JSON.stringify(messageBody),
+        QueueUrl: queueURL,
+    };
+       
+    sqs.sendMessage(params, (err, data) => {
+        if (err) {
+           return res.send({ status: err });
+        } else {
+           return res.send({ status: "OK" });
+        }
+    });
 });
 
 app.listen(PORT, HOST);
--- tenantreader/server.js
+++ tenantreader/server.js
@@ -1,46 +1,57 @@
-var AWS = require('aws-sdk/global');
-var SQS = require('aws-sdk/clients/SQS');
+const AWS = require('aws-sdk');
 
-var myCredentials = new AWS.Credentials("x", "x");
-
-var sqs = new AWS.SQS({
+const myCredentials = new AWS.Credentials("x", "x");
+const sqs = new AWS.SQS({
     apiVersion: '2012-11-05',
     credentials: myCredentials,
     region: "none",
     endpoint: "http://sqs:9324"
 });
 
-var queueURL = "http://sqs:9324/queue/queue1";
-
-var params = {
- AttributeNames: [
-    "SentTimestamp"
- ],
- MaxNumberOfMessages: 1,
- MessageAttributeNames: [
-    "All"
- ],
- QueueUrl: queueURL,
- VisibilityTimeout: 0,
- WaitTimeSeconds: 0
+const queueURL = "http://sqs:9324/queue/newtenants";
+const params = {
+    AttributeNames: [
+        "SentTimestamp"
+    ],
+    MaxNumberOfMessages: 10,
+    MessageAttributeNames: [
+        "All"
+    ],
+    QueueUrl: queueURL,
+    VisibilityTimeout: 30,
+    WaitTimeSeconds: 0
 };
 
 const timeoutPromise = (timeout) => new Promise((resolve) => setTimeout(resolve, timeout));
 const runFor = async (interval) => {
+    let numSubscription = 0;
     while (true) {
         await timeoutPromise(interval);
 
-        sqs.receiveMessage(params, function(err, data) {
+        sqs.receiveMessage(params, (err, data) => {
             if (err) {
               console.log("Receive Error", err);
             } else if (data.Messages) {
-              console.log("Message: ", data.Messages[0]);
-            } else {
-              console.log("Empty!");
+                for (let message of data.Messages) {
+                    const deleteParams = {
+                        QueueUrl: queueURL,
+                        ReceiptHandle: message.ReceiptHandle
+                    };
+                    // do job in visivilitytimeout then try delete
+                    numSubscription ++;
+                    console.log(`${numSubscription}: Ret mailboxes from: ${message.Body} - ${message.MessageId}`);
+                    sqs.deleteMessage(deleteParams, (err, data) => {
+                        if (err) {
+                            console.log("Delete Error", err);
+                        } else {
+                            // deleted
+                        }
+                    });
+                }
             }
         });
     }
 };
 
 console.log("RUN!");
-runFor(5000);
+runFor(1000);
